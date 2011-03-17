aedaf1778b836c613027ef636eb2552dcff79cf9
Adam Lesperance <lespea@cpan.org>
2011-03-17 11:13:59 +0000

    Import of LESPEA/Dist-Zilla-Plugin-SpellingCommonMistakesTests-1.000001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-SpellingCommonMistakesTests
    gitpan-cpan-version:      1.000001
    gitpan-cpan-path:         LESPEA/Dist-Zilla-Plugin-SpellingCommonMistakesTests-1.000001.tar.gz
    gitpan-cpan-author:       LESPEA
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/SpellingCommonMistakesTests.pm
+++ lib/Dist/Zilla/Plugin/SpellingCommonMistakesTests.pm
@@ -4,7 +4,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::SpellingCommonMistakesTests;
 BEGIN {
-  $Dist::Zilla::Plugin::SpellingCommonMistakesTests::VERSION = '1.000000';
+  $Dist::Zilla::Plugin::SpellingCommonMistakesTests::VERSION = '1.000001';
 }
 BEGIN {
   $Dist::Zilla::Plugin::SpellingCommonMistakesTests::AUTHORITY = 'cpan:LESPEA';
@@ -37,13 +37,13 @@ Dist::Zilla::Plugin::SpellingCommonMistakesTests - Release tests for common POD 
 
 =head1 VERSION
 
-version 1.000000
+version 1.000001
 
 =head1 SYNOPSIS
 
 In your dist.ini file:
 
-    [PodSpellingTests]
+    [SpellingCommonMistakesTests]
 
 =head1 DESCRIPTION
 
